f72cd103db93b44c519d21babb82e280b14595c8
XandraN09 <123044475+IsisXN@users.noreply.github.com>
2023-05-02 21:07:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-function init() {
-  //alert('it works');
+function init(){
   var el = document.getElementById('canvas');
-  var myLocation = new google.maps.LatLng(20.69152746800962, -103.33691327737087);
+  var myLocation = new google.maps.LatLng(15.534637544573304, -89.95940777671223);
   var mapOptions = {
     center: myLocation,
-	zoom: 16,
-	mapTypeId: google.maps.MapTypeId.SATELLITE,
-	mapTypeControlOptions: {
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
       position: google.maps.ControlPosition.BOTTOM_CENTER
     }
   };
-  
+
   var myMap = new google.maps.Map(el, mapOptions);
-  
+
   var marker = new google.maps.Marker({
     position: myLocation,
     map: myMap,
     animation: google.maps.Animation.DROP,
-	icon: {
-		url: 'images/icon.png',
-		scaledSize: new google.maps.Size(60,60)
+    icon: {
+      url: 'media/quetzal.png',
+      scaledSize: new google.maps.Size(60, 60) 
     }
   });
-  
-  
-  var contentString = '<h2>Guadalajara</h2><p>This is one of the states in Mexico</p>'
-  
+
+
+  var contentString = '<h2>Semuc Champey</h2><p>The national bird of Guatemala is the Quetzal</p>'
+
   var infowindow = new google.maps.InfoWindow({
-    content: contentString,
+    content: contentString
   });
-  
+
   google.maps.event.addListener(marker, 'mouseover', function() {
+	  infowindow.open(myMap, marker);
+  });
+
+  
+  marker.addListener('click', function() {
     infowindow.open(myMap, marker);
   });
-
-
-  marker.addListener('click', function() {
-	  infowindow.open(myMap,marker);
-  });
 }
+
 google.maps.event.addDomListener(window, 'load', init);
